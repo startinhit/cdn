dbec9dbd580c07e6f2a2c22198d819978246fc50
tuantinh <39516314+tuantinh@users.noreply.github.com>
2020-04-04 17:55:37 +0700

    Update main-paper.js

--- js/main-paper.js
+++ js/main-paper.js
@@ -1181,6 +1181,12 @@ $(function() {
     });
 });
 
+$('.collapsible-item--title').click(function (e) {
+  $(this).toggleClass('active').siblings('.collapsible-item--content').slideToggle(500);
+  $(this).parent().siblings().children('.collapsible-item--title').removeClass('active');
+  $(this).parent().siblings().children('.collapsible-item--content').slideUp(500);
+})
+                                              
 /*! delete ?m=1 */
 var uri;if((uri=window.location.toString()).indexOf("%3D","%3D")>0){var clean_uri=uri.substring(0,uri.indexOf("%3D"));window.history.replaceState({},document.title,clean_uri)}if((uri=window.location.toString()).indexOf("%3D%3D","%3D%3D")>0){clean_uri=uri.substring(0,uri.indexOf("%3D%3D"));window.history.replaceState({},document.title,clean_uri)}if((uri=window.location.toString()).indexOf("&m=1","&m=1")>0){clean_uri=uri.substring(0,uri.indexOf("&m=1"));window.history.replaceState({},document.title,clean_uri)}if((uri=window.location.toString()).indexOf("?m=1","?m=1")>0){clean_uri=uri.substring(0,uri.indexOf("?m=1"));window.history.replaceState({},document.title,clean_uri)}var protocol=window.location.protocol.replace(/\:/g,"");if("http"==protocol){var url=window.location.href.replace("http","https");window.location.replace(url)};
 
